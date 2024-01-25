98d0b00c92bef10345bc32a667bd28178477b462
JDev000 <jdeveloper2022@gmail.com>
2024-01-25 17:38:49 +0800

    updated with appstate

--- server/index.js
+++ server/index.js
@@ -1,8 +1,29 @@
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
 
-app.use('/', (req, res)=>{
-    res.send("Server is running...");
-});
+// app.use('/', (req, res)=>{
+//     res.send("Server is running...");
+// });
 
-app.listen(5000, console.log("Server running on PORT 5000"));
+// app.listen(5000, console.log("Server running on PORT 5000"));
+
+
+
+
+const { readFileSync } = require('fs');
+const login = require("facebook-chat-api");
+
+loginPath = { appState: JSON.parse(readFileSync(__dirname + "/appstate.json", "utf-8")) };
+
+login(loginPath, (err,api) => {
+    if (err) return console.error(err);
+    
+   api.listenMqtt((error,message)=>{
+    if (error) return console.error(err);
+
+    // api.sendMessage(message.body, message.threadID, message.messageID)
+
+    if(message.body == "hi") 
+        api.sendMessage("How's your day?", message.threadID, message.messageID)
+   })
+})
